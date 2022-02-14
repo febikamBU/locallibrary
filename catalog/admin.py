5f6e51c3fa6678bbe3049da31638b353dd366f1a
Franklyn Ebikam <febikam@bu.edu>
2022-02-13 21:58:30 -0600

    Registered ModelAdmin class

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,7 +4,7 @@ from .models import Author, Genre, Book, BookInstance, Language
 # Register the Admin classes for Author using the decorator
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
 # Register the Admin classes for Genre using the decorator
 @admin.register(Genre)
